0d573eee83e94018a7b48ba44596bc8ae33dd36e
RainMakerDEVELOP <winekein@naver.com>
2022-11-01 18:00:16 +0900

    첫번째 체크인

---
+++ PcWrnCheckerSvr.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("-------------------------------------------------")
+	fmt.Println("---------- Pc Wrn Checker Server Start ----------")
+	fmt.Println("-------------------------------------------------")
+
+	fmt.Println("-------------------------------------------------")
+	fmt.Println("---------- Pc Wrn Checker Server End   ----------")
+	fmt.Println("-------------------------------------------------")
+}
