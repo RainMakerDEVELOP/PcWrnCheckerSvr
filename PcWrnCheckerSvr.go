e8a6814d006e1c4de0d87a5f21399e144501619f
RainMakerDEVELOP <winekein@naver.com>
2022-11-04 19:27:36 +0900

    2022.11.04 #03

--- PcWrnCheckerSvr.go
+++ PcWrnCheckerSvr.go
@@ -2,16 +2,76 @@ package main
 
 import (
 	pwc_svr_arg "PcWrnChecker/PcWrnCheckerSvr/pwcpkg"
+	"bufio"
 	"fmt"
+	"net"
 )
+
+const addr = "localhost:8888"
+
+var m_mapClientInfo map[string]pwc_svr_arg.PwcArg
+
+func echoBackCapitalized(conn net.Conn) {
+	// conn에 리더(reader)를 설정한다(io.Reader)
+	reader := bufio.NewReader(conn)
+
+	remoteAddr := conn.RemoteAddr().String()
+
+	fmt.Printf("remoteAddr : %s\n", remoteAddr)
+
+	// 해당 주소가 목록에 있는지 조사
+	vClientInfo, ok := m_mapClientInfo[remoteAddr]
+
+	if ok == true { // 해당 주소가 목록에 있으면
+	} else { // 해당 주소가 목록에 없으면
+		// 읽어온 데이터의 첫 줄을 가져온다.
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("error reading data : %s\n", err.Error())
+			return
+		}
+
+		// 해당 주소의 모니터링 정보에 모니터링하고자 하는 항목이 있는지 조사
+		vItem, ok := vClientInfo.ExistClient(data)
+
+		if ok == true { // 해당 항목이 있으면, 항목의 모니터링 값을 추가
+			fmt.Printf("ExistClient vItem.StartTime = '%s'\n", vItem.StartTime.String())
+		} else { // 해당 항목이 없으면 추가
+			addRet := vClientInfo.AddClient(data)
+
+			fmt.Printf("AddClient Ret = '%d'\n", addRet)
+		}
+		// 출력한 다음 데이터를 다시 보낸다.
+		// fmt.Printf("Received : %s\n", data)
+		// conn.Write([]byte(strings.ToUpper(data)))
+		// m_mapClientInfo[remoteAddr] =
+	}
+}
 
 func main() {
 	fmt.Println("-------------------------------------------------")
 	fmt.Println("---------- Pc Wrn Checker Server Start ----------")
 	fmt.Println("-------------------------------------------------")
 
-	bRet := pwc_svr_arg.AddClient("a")
-	fmt.Println(bRet)
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer ln.Close()
+	fmt.Printf("listening on : %s\n", addr)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("encountered an error accepting connection : %s\n", err.Error())
+
+			// 오류가 있으면 다시 시도한다
+			continue
+		}
+
+		// 이 작업을 비동기로 처리하면 잠재적으로 워커 풀을 위해 좋은 사용 사례가 될 것이다
+		go echoBackCapitalized(conn)
+	}
 
 	fmt.Println("-------------------------------------------------")
 	fmt.Println("---------- Pc Wrn Checker Server End   ----------")
--- pwcpkg/pwc_svr_arg.go
+++ pwcpkg/pwc_svr_arg.go
@@ -6,20 +6,24 @@ import (
 
 // 모니터링할 PC 정보 상세( CPU 점유율, RAM 사용량 등)
 type PwcArg_Item struct {
-	ItemName  string
 	StartTime time.Time
 	EndTime   time.Time
 }
 
 type PwcArg struct {
-	ClientIPAddr    string
-	Monitoring_Item []PwcArg_Item
+	mapMon_Item map[string]PwcArg_Item
 }
 
-func AddClient(string) bool {
-	return false
+func (pa PwcArg) AddClient(itemName string) bool {
+	var pi PwcArg_Item
+	pi.StartTime = time.Now() // 신규 추가시에는 시작 시간만 기록한다
+	pa.mapMon_Item[itemName] = pi
+
+	return true
 }
 
-func ExistClient(string) bool {
-	return false
+func (pa PwcArg) ExistClient(itemName string) (PwcArg_Item, bool) {
+	vItemMap, ok := pa.mapMon_Item[itemName]
+
+	return vItemMap, ok
 }
